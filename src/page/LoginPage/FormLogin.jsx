3bcd3f2db772faa4b07a5a46d5aab1bf347ad8cd
Trieu Vy <trtrvy1211@gmail.com>
2024-05-26 16:47:08 +0700

    fix formlogin

--- src/page/LoginPage/FormLogin.jsx
+++ src/page/LoginPage/FormLogin.jsx
@@ -19,7 +19,7 @@ const FormLogin = () => {
       .post("/api/Users/signin", values)
       .then((res) => {
         // chuyển hướng user về home sau khi đăng nhập thành công
-        navigate("/project");
+        navigate("/projectmanager");
         // đẩy data lên redux
         dispatch(setUser(res.data.content));
         message.success("Login successfully");
